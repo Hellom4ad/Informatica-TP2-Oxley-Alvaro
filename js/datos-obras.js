6b69f95ae8302ce42d7273b8d2b88151b9c10e96
Alvaro Oxley <alvarooxley@gmail.com>
2025-11-02 18:35:01 -0300

    Se agregaron descripciones a las obras.

--- js/datos-obras.js
+++ js/datos-obras.js
@@ -3,42 +3,54 @@ const todasLasObras = [
         id: 1,
         titulo: 'Pesadilla de los injustos',
         img: 'img/obras/pesadilla-de-los-injustos.jpg',
-        desc: 'Fecha: 1961, soporte: sobre tela.'
+        desc: 'Fecha: 1961, soporte: sobre tela.',
+        info: 'En esa ocasión, Berni obtuvo el Gran Premio de Grabado y Dibujo con la serie de xilocollages sobre ese mismo personaje, Juanito Laguna. La adquisición de Juanito Laguna aprende a leer estuvo directamente vinculada a esta premiación inesperada en el foro internacional de las artes visuales más tradicional y prestigioso: los trámites de compra se iniciaron en agosto de 1962, un mes antes de que la Bienal cerrara sus puertas. La reciente adquisición de Pesadilla de los injustos vino a ampliar la representación de este momento clave en la trayectoria del artista rosarino. Pero la coyuntura de esta entrada al acervo público fue bien diferente. Al momento de la Bienal, Berni tenía 57 años de edad y una larga trayectoria de pintor realista (y comunista). Su potencial como exponente internacional del arte argentino despuntó a partir de la premiación veneciana, que contribuyó a la proyección de su obra por fuera de las fronteras nacionales durante los años que siguieron. En este sentido, la compra de una obra contemporánea del ganador del premio veneciano constituía una prueba de las bondades del arte nacional pero también una apuesta a constituir un canon. En cambio, con el auge de mercado del llamado arte latinoamericano a comienzos de los años 90, por un lado, y a partir de una serie de exhibiciones de gran relevancia, Berni constituye hoy uno de los nombres insoslayables del arte argentino. '
     },
     {
         id: 2,
         titulo: 'La siesta',
         img: 'img/obras/la-siesta.jpg',
-        desc: 'Fecha: 1943 soporte: sobre tela.'
+        desc: 'Fecha: 1943 soporte: sobre tela.',
+        info: 'Testigo de los momentos más dramáticos de la historia del siglo XX, Antonio Berni hace una denuncia de los efectos de las guerras, de los vicios y peligros de la sociedad de consumo que convierten en víctimas a los seres humanos. El constante movimiento pendular entre la realidad que le toca vivir y aquellos temas consagrados por la historia del arte como el apocalipsis, las madonnas o las crucifixiones, entre otros, marcan la originalidad de sus obras. Berni reinterpreta la iconografía cristiana tradicional incorporando la vida cotidiana y los problemas sociales de su época. Las citas a los temas religiosos se  hacen presentes en distintas etapas de su producción, como la madonna y el niño y la Sacra conversazione en Chacareros (1935), el nacimiento de Cristo en La siesta (1943), el lamento sobre Cristo muerto en Medianoche en el mundo (1936-1937), la alusión a la Sagrada Familia en Sol de domingo (1941), la última cena en Domingo en la chacra (o El almuerzo) (1945-1971), la lamentación de María en Obrero muerto (1948), El obrero encadenado (o San Sebastián) (1949), el entierro en Obrero muerto o El velatorio (1949). A éstas le seguirá una serie de obras de  los años ’80: la iconografía de la Crucifixión en Cristo en el departamento (1980) y el Apocalipsis (1981) en las pintura murales de la capilla San Luis Gonzaga de Las Heras, al igual que Magdalena (1980), Cristo en el garag” (1981). En estas obras Berni se vale de la iconografía religiosa pero con otro contenido, en alusión a problemáticas sociales.(1)'
     },
     {
         id: 3,
         titulo: 'Primeros pasos',
         img: 'img/obras/primeros-pasos.jpg',
-        desc: 'Fecha: 1936, soporte: sobre tela.'
+        desc: 'Fecha: 1936, soporte: sobre tela.',
+        info: '​Luego de una decisiva estadía europea en la que frecuentó las innovaciones de las vanguardias, las tendencias del retorno al orden y las grandes tradiciones figurativas de Occidente, Antonio Berni regresó a Rosario a fines de 1931. Si bien siguió transitando las experiencias surrealistas que había cultivado durante el último tramo de su permanencia en París, rápidamente se encaminó hacia un nuevo realismo (1) que se diferenciaba tanto de los precedentes decimonónicos como del realismo socialista propiciado desde la Unión Soviética (2). Se trataba, entonces, de una versión moderna y heterodoxa que el artista comenzó a practicar junto a jóvenes discípulos en la Mutualidad Popular de Estudiantes y Artistas Plásticos; agrupación recientemente fundada en Rosario y cuyo ciclo se extendió con intensidad hasta la radicación del maestro en Buenos Aires hacia 1936 (3). Como parte de una doble militancia por el arte nuevo y por una nueva sociedad, en buena medida estimulada por la presencia de David Alfaro Siqueiros en 1933, (4) el grupo desplegó experiencias murales, decoraciones para actos políticos y piezas gráficas oscilantes entre la propaganda y la estampa tradicional. Sin embargo, la pintura de caballete, sobre todo de gran formato, siguió desarrollándose en diversas escalas y modalidades, desde la pintura mural transportable hasta los cuadros concebidos para una contemplación íntima y reposada. Por tal motivo, afloraban en sus realizaciones los acontecimientos contemporáneos y los episodios que arrojaba la crónica periodística, así como la épica de la vida cotidiana y el retrato monumentalizado.'
+
     },
     {
         id: 4,
         titulo: 'Figura de niña',
         img: 'img/obras/figura-de-niña.jpg',
-        desc: 'Fecha: 1910-1945, soporte: sobre tela.'
+        desc: 'Fecha: 1910-1945, soporte: sobre tela.',
+        info: 'Sin información adicional'
+
     },
     {
         id: 5,
         titulo: 'Cristo en el departamento',
         img: 'img/obras/cristo-en-el-departamento.jpg',
-        desc: 'Fecha: 1981, soporte: sobre tela.'
+        desc: 'Fecha: 1981, soporte: sobre tela.',
+        info: 'Sin Información adicional'
+
     },
     {
         id: 6,
         titulo: 'Juanito Laguna aprende a leer',
         img: 'img/obras/juanito-laguna-aprende-a-leer.jpg',
-        desc: 'Fecha: 1961, soporte: sobre tela.'
+        desc: 'Fecha: 1961, soporte: sobre tela.',
+        info: 'En esa ocasión, Berni obtuvo el Gran Premio de Grabado y Dibujo con la serie de xilocollages sobre ese mismo personaje, Juanito Laguna. La adquisición de Juanito Laguna aprende a leer estuvo directamente vinculada a esta premiación inesperada en el foro internacional de las artes visuales más tradicional y prestigioso: los trámites de compra se iniciaron en agosto de 1962, un mes antes de que la Bienal cerrara sus puertas. La reciente adquisición de Pesadilla de los injustos vino a ampliar la representación de este momento clave en la trayectoria del artista rosarino. Pero la coyuntura de esta entrada al acervo público fue bien diferente. Al momento de la Bienal, Berni tenía 57 años de edad y una larga trayectoria de pintor realista (y comunista). Su potencial como exponente internacional del arte argentino despuntó a partir de la premiación veneciana, que contribuyó a la proyección de su obra por fuera de las fronteras nacionales durante los años que siguieron. En este sentido, la compra de una obra contemporánea del ganador del premio veneciano constituía una prueba de las bondades del arte nacional pero también una apuesta a constituir un canon. En cambio, con el auge de mercado del llamado arte latinoamericano a comienzos de los años 90, por un lado, y a partir de una serie de exhibiciones de gran relevancia, Berni constituye hoy uno de los nombres insoslayables del arte argentino.'
+
     },
     {
         id: 7,
         titulo: 'El tanque blanco',
         img: 'img/obras/el-tanque-blanco.jpg',
-        desc: 'Fecha: 1910-1945, soporte: sobre tela.'
+        desc: 'Fecha: 1910-1945, soporte: sobre tela.',
+        info: 'Sin información adicional'
+
     }
 ];
